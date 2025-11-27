52c30adcbf73edf6b5ae3ed72d23337ef741c477
vishalsharma659615 <vishalsharma659615@gmail.com>
2025-11-27 12:54:14 +0000

    added the login page design.

---
+++ smartlib/lib/home_pages/user home.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:smartlib/pages/Book_detail.dart';
+
+class Student_home extends StatefulWidget {
+  const Student_home({super.key,required this.title});
+  final String title;
+
+  @override
+  State<Student_home> createState() => _Student_homeState();
+}
+
+class _Student_homeState extends State<Student_home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.tealAccent.shade400,
+        elevation: 0.0,
+        width: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+
+                GestureDetector(
+                  onTap: _incrementCounter,
+                  child:Row(
+                    children: [
+                      Icon(Icons.settings),
+                      Text("Profile")
+                      ],
+                    ),
+                  ),
+              ],
+            )
+          ],
+          ),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top:17.0,bottom: 17.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Card(
+                    color: const Color.fromARGB(255, 83, 246, 205),
+                    elevation: 0.0,
+                    child:Padding(
+                      padding: const EdgeInsets.only(top:15.0,bottom: 15.0),
+                      child: Container(
+                        width:125,
+                        child: Column(children: [
+                          const Text('Books Issued'),
+                          Text(
+                            '$_counter',
+                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),  
+                          ),
+                        ],),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: const Color.fromARGB(255, 83, 246, 205),
+                    elevation: 0.0,
+                    child:Padding(
+                      padding: const EdgeInsets.only(top:15.0,bottom: 15.0),
+                      child: Container(
+                        width:125,
+                        child: Column(children: [
+                          const Text('Total Fine Issued'),
+                          Text(
+                            'Rs.$_counter',
+                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),  
+                          ),
+                        ],),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Text("Issued Books Details",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),          
+            const SizedBox(height: 10,),
+            SizedBox(
+              height: 400,
+              child: Expanded(
+                child: SingleChildScrollView( 
+                  child: Column( 
+                  children: <Widget>[
+                    ListTile(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => BookDetail()));
+                      },
+                      leading: CircleAvatar(),
+                      title: Text("Book 1"),
+                      subtitle: Text("author 1"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name"),
+                      subtitle: Text("author"),
+                      trailing: Text("Issue date"),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(),
+                      title: Text("Book Name1"),
+                      subtitle: Text("author1"),
+                      trailing: Text("Issue date1"),
+                    ),
+                    ]
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15,),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.pushNamed(context, '/books_details');
+        },
+        tooltip: 'ADD BOOK',
+        child: const Icon(Icons.bookmark_add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ smartlib/lib/pages/login.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Login_SL extends StatefulWidget {
+  const Login_SL({super.key});
+
+  @override
+  State<Login_SL> createState() => _Login_SLState();
+}
+
+class _Login_SLState extends State<Login_SL> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        automaticallyImplyLeading: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(25)),border: Border.all(color: Colors.black)),
+              child: IconButton(onPressed: (){},icon: Icon(Icons.remove_red_eye))
+              ),
+          )
+        ],
+      ),
+      extendBody: true,
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.875),
+          gradient:LinearGradient(
+            colors: [
+              Colors.blueAccent,
+              Colors.teal,
+              Colors.tealAccent,
+              Colors.greenAccent,
+              Colors.lightGreen,
+              Colors.green,
+              Colors.brown.shade300
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ), 
+        ),
+        child: Expanded(
+          flex: 1,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25,right: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                  Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Text("LOGIN",textAlign: TextAlign.center,style: GoogleFonts.pacifico(fontSize: 40,fontWeight: FontWeight.bold),),
+                        ),
+                        TextFormField(decoration: InputDecoration(hintText: "Username",border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),showCursor: false,enableSuggestions: false),
+                        const SizedBox(height: 20),
+                        TextFormField(obscureText: true,obscuringCharacter: "*",decoration: InputDecoration(hintText: "Password",border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),showCursor: false,enableSuggestions: false),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            TextButton(onPressed: (){Navigator.pushNamed(context, "/home");},style: ButtonStyle(fixedSize: WidgetStateProperty.all(Size(130, 40)),backgroundColor: WidgetStateProperty.all(Colors.teal),elevation: WidgetStatePropertyAll(20), ), child: Text("Sign-In",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)),),
+                            TextButton(onPressed: (){Navigator.pushNamed(context, "/home");},style: ButtonStyle(fixedSize: WidgetStateProperty.all(Size(130, 40)),backgroundColor: WidgetStateProperty.all(Colors.teal),elevation: WidgetStatePropertyAll(20),), child: Text("Register-Now",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
+                          ],
+                        )
+                      ],
+                    ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomSheet: Container(
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.875),
+          gradient:LinearGradient(
+            colors: [
+              Colors.lightGreen,
+              Colors.green,
+              Colors.brown.shade300
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ), 
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 20.0),
+          child: Text("Forget password ??",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold, backgroundColor: Colors.transparent),),
+        ),
+      ),
+    );
+  }
+}
